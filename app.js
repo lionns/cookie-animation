ce31305593678dee09ff309ff35df9131dd0200e
Sebastian Leon <juans.leonv@icloud.com>
2021-12-17 18:56:53 -0500

    Gsap timeline animation

--- app.js
+++ app.js
@@ -1,2 +1,35 @@
-gsap.fromTo(".text", { opacity: 0, y: 20 }, { opacity: 1, y: 0, duraction: 1 });
+const tl = gsap.timeline({ defaults: { duration: 0.75, ease: "power1.out" } });
+
+tl.fromTo(
+  ".cookie-container",
+  { scale: 0 },
+  { scale: 1, ease: "elastic.out(1, 0.4)", duration: 1.5 }
+);
+tl.fromTo(
+  ".cookie",
+  {
+    opacity: 0,
+    x: -50,
+    rotation: "-45deg",
+  },
+  {
+    opacity: 1,
+    x: 0,
+    rotation: "0deg",
+  },
+  "<50%"
+);
+tl.fromTo(".text", { x: 30, opacity: 0 }, { x: 0, opacity: 1 }, "<");
+
+const button = document.querySelector("button");
+button.addEventListener("click", () => {
+  gsap.to(".cookie-container", {
+    opacity: 0,
+    y: 100,
+    duration: 0.75,
+    ease: "power1.out",
+  });
+});
+
+// gsap.fromTo(".text", { opacity: 0, y: 20 }, { opacity: 1, y: 0, duraction: 1 });
 // gsap.to(".text", { y: 200, opacity: 0, duration: 1 });
